d4b1ef384b4cc099fe405790f95c43d44e5a35b8
Nicholas Rockwell <70916509+grandpaku@users.noreply.github.com>
2021-10-19 13:53:44 -0700

    adding nums and js return result

--- sumjava.js
+++ sumjava.js
@@ -1,8 +1,9 @@
 function buttonClicked()
 {
     var value1 = document.getElementById('number1'),value;
+    value1 = parseInt(value1);
     var value2 = document.getElementById('number2'),value;
-
+    value2 = parseInt(value2);
     var result = value1 + value2;
-    document.getElementByID('result')
+    document.getElementByID('result').value = result;
 }
